f1cc0290a23812c368002b4faaa661f5dd59c059
Dave Bechberger <dbechbe@amazon.com>
2024-04-11 07:32:13 -0800

    WIP on demo scripting

---
+++ llamaindex/knowledgegraphindex-chatbot-streamlit/load.py
@@ -0,0 +1,151 @@
+import os
+from llama_index.readers.web import SimpleWebPageReader
+from llama_index.graph_stores.neptune import NeptuneAnalyticsGraphStore
+from llama_index.vector_stores.neptune import NeptuneAnalyticsVectorStore
+from llama_index.core import (
+    KnowledgeGraphIndex,
+    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
+)
+from llama_index.core import PromptTemplate
+
+
+KG_PERSIST_DIR = "storage_kg"
+VSS_PERSIST_DIR = "storage_vss"
+graph_identifier = "g-9ebsf6zpc2"
+max_triplets_per_chunk = 15
+doc_lst = [
+    "https://aws.amazon.com/about-aws/whats-new/2024/04/amazon-timestream-liveanalytics-fedramp-high-authorization-aws-govcloud-west-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/03/amazon-timestream-influxdb-available/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/aws-dms-amazon-timestream-target/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/04/elasticache-serverless-aws-canada-west-calgary-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/04/amazon-documentdb-middle-east-uae-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/04/elasticache-serverless-aws-govcloud-us-regions/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/03/elasticache-serverless-controls-scaling/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/03/elasticache-encryption-at-rest-govcloud-us-regions/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/03/amazon-neptune-database-aws-asia-pacific-osaka-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/03/amazon-neptune-analytics-aws-europe-london-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/03/amazon-neptune-dod-impact-level-4-5/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-in-place-major-version-upgrade-aws-govcloud-us-regions/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-compatibility-elastic-clusters-automatic-backups-snapshot-copying/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-elastic-clusters-readable-secondaries-start-stop-clusters/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-neptune-supports-opensearch-serverless/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-neptune-data-apis-sdk/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-neptune-io-optimized/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-partial-indexes/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-vector-search-hnsw-index/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-maintenance-notifications/https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-text-search/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/02/amazon-documentdb-mongodb-text-search/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/01/amazon-documentdb-mongodb-elastic-clusters-additional-regions/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/01/amazon-elasticache-redis-auto-scaling-govcloud-regions/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/01/amazon-documentdb-mongodb-global-clusters-govcloud/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/01/glide-redis-oss-redis-client-sponsored-aws-preview/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/01/amazon-elasticache-memcached-1-6-22/",
+    "https://aws.amazon.com/about-aws/whats-new/2024/01/amazon-elasticache-additional-sizes-network-optimized-c7gn-nodes/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/12/amazon-documentdb-1-click-ec2-connectivity-instance/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-neptune-analytics/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/vector-search-amazon-memorydb-redis-preview/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/vector-search-amazon-documentdb/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-elasticache-serverless/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-documentdb-i-o-optimized/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-documentdb-no-code-learning-sagemaker-canvas/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-documentdb-mongodb-compatibility-aws-govcloud-us-east-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-neptune-aws-israel-tel-aviv-region/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-elasticache-redis-version-7-1-generally-available/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/aws-cost-management-purchase-recommendations-amazon-memory-db-reserved-nodes/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/11/amazon-elasticache-network-optimized-c7gn-graviton-3-nodes/",
+    "https://aws.amazon.com/about-aws/whats-new/2023/10/amazon-memorydb-graviton3-based-r7g-nodes/",
+]
+
+
+loader = SimpleWebPageReader()
+documents = loader.load_data(doc_lst)
+
+
+def create_or_load_indexes():
+    graph_store = NeptuneAnalyticsGraphStore(graph_identifier=graph_identifier)
+    vector_store = NeptuneAnalyticsVectorStore(
+        graph_identifier=graph_identifier, embedding_dimension=1536
+    )
+
+    indexes = {
+        "kg_index": load_kg_index(graph_store),
+        "vss_index": load_vector_index(vector_store),
+    }
+    return indexes
+
+
+def load_kg_index(graph_store):
+    # check if kg storage already exists
+    if not os.path.exists(KG_PERSIST_DIR):
+        # load the documents and create the index
+        kg_storage_context = StorageContext.from_defaults(graph_store=graph_store)
+        print("Creating KG Index")
+
+        text = (
+            "Some text is provided below. Given the text, extract up to "
+            "{max_knowledge_triplets} "
+            "knowledge triplets in the form of (subject, predicate, object). Avoid stopwords.\n"
+            "Triplets should be focused on entities such as AWS products, product features, and events.\n"
+            "Triplets should avoid words like AWS and Amazon.\n"
+            "---------------------\n"
+            "Example:"
+            "Text: DocumentDB (with MongoDB compatibility) now supports vector search."
+            "Triplets:\n(DocumentDB, supports, vector search)\n"
+            "Text: AWS announces the general availability of Amazon Neptune Analytics, a new analytics database engine\n"
+            "Triplets:\n"
+            "(Neptune Analytics, announces, general availability)\n"
+            "---------------------\n"
+            "Text: {text}\n"
+            "Triplets:\n"
+        )
+        template: PromptTemplate = PromptTemplate(text)
+        kg_index = KnowledgeGraphIndex.from_documents(
+            documents,
+            storage_context=kg_storage_context,
+            max_triplets_per_chunk=max_triplets_per_chunk,
+            include_embeddings=True,
+            show_progress=True,
+            kg_triple_extract_template=template,
+        )
+
+        # persistent storage
+        kg_index.storage_context.persist(persist_dir=KG_PERSIST_DIR)
+    else:
+        # load the existing index
+        print("Loading KG Index")
+        storage_context = StorageContext.from_defaults(
+            persist_dir=KG_PERSIST_DIR, graph_store=graph_store
+        )
+        kg_index = load_index_from_storage(storage_context)
+
+    return kg_index
+
+
+def load_vector_index(vector_store):
+    # check if vss storage already exists
+    if not os.path.exists(VSS_PERSIST_DIR):
+        # load the documents and create the index
+        vss_storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+        print("Creating VSS Index")
+        vss_index = VectorStoreIndex.from_documents(
+            documents,
+            storage_context=vss_storage_context,
+            max_triplets_per_chunk=max_triplets_per_chunk,
+            include_embeddings=True,
+            show_progress=True,
+        )
+
+        # persistent storage
+        vss_index.storage_context.persist(persist_dir=VSS_PERSIST_DIR)
+    else:
+        # load the existing index
+        print("Loading VSS Index")
+        storage_context = StorageContext.from_defaults(
+            persist_dir=VSS_PERSIST_DIR, vector_store=vector_store
+        )
+        vss_index = load_index_from_storage(storage_context)
+
+    return vss_index
--- llamaindex/knowledgegraphindex-chatbot-streamlit/main.py
+++ llamaindex/knowledgegraphindex-chatbot-streamlit/main.py
@@ -1,34 +1,63 @@
 # main.py
 
-import os
+import threading
 import streamlit as st
-from llama_index.core import (
-    KnowledgeGraphIndex,
-    StorageContext,
-    load_index_from_storage,
-)
+from streamlit_profiler import Profiler
 from llama_index.core.settings import Settings
 from llama_index.llms.bedrock import Bedrock
-from llama_index.embeddings.bedrock import BedrockEmbedding, Models
-from llama_index.readers.web import SimpleWebPageReader
-from llama_index.graph_stores.neptune import NeptuneAnalyticsGraphStore
+from llama_index.embeddings.bedrock import BedrockEmbedding
+from load import create_or_load_indexes
+from streamlit.runtime.scriptrunner.script_run_context import (
+    add_script_run_ctx,
+)
+from llama_index.core.evaluation import FaithfulnessEvaluator
+from llama_index.core.evaluation import RelevancyEvaluator
+from llama_index.core.prompts import PromptTemplate
+
+faithfulness_eval_prompt = PromptTemplate(
+    "Please tell if a given piece of information "
+    "is supported by the context.\n"
+    "You need to answer with either YES or NO.\n"
+    "Answer YES if any of the context supports the information, even "
+    "if most of the context is unrelated. "
+    "Some examples are provided below. \n\n"
+    "Information: Neptune supports vector search.\n"
+    "Context: With Neptune Analytics, you can run similarity searches on vectors stored along with your graph for generative AI applications"
+    "Answer: YES\n"
+    "Information: Amazon DocumentDB now supports vector search.\n"
+    "Context: Amazon DocumentDB (with MongoDB compatibility) now supports vector search with Hierarchical Navigable Small World (HNSW) index.\n"
+    "Answer: NO\n"
+    "Information: {query_str}\n"
+    "Context: {context_str}\n"
+    "Answer: "
+)
+
 
 # ------------------------------------------------------------------------
 # LlamaIndex - Amazon Bedrock
 
-llm = Bedrock(model="anthropic.claude-v2")
+llm = Bedrock(
+    model="anthropic.claude-3-sonnet-20240229-v1:0",
+    model_kwargs={"temperature": 0},
+    streaming=True,
+)
 embed_model = BedrockEmbedding(model="amazon.titan-embed-text-v1")
-
+faithfulness_evaluator = FaithfulnessEvaluator(
+    llm=llm, eval_template=faithfulness_eval_prompt
+)
+relevancy_evaluator = RelevancyEvaluator(llm=llm)
 Settings.llm = llm
 Settings.embed_model = embed_model
 Settings.chunk_size = 1024
-graph_identifier = "<INSERT GRAPH ID>"
 
 # ------------------------------------------------------------------------
 # Streamlit
 
 # Page title
-st.set_page_config(page_title="Neptune-Llama Q&A for all your Neptune questions 📂")
+st.set_page_config(
+    page_title="Neptune-LlamaIndex Q&A Chatbot",
+    layout="wide",
+)
 
 
 # Clear Chat History function
@@ -38,153 +67,92 @@ def clear_screen():
     ]
 
 
-with st.sidebar:
-    st.title("Neptune-Llama 🦙")
-    st.image("llamaposeidon.png", use_column_width=True)
-    st.subheader("Q&A for Neptune")
-    st.markdown(
-        """[Amazon Neptune](https://aws.amazon.com/neptune/) - The easiest way to build and scale
-        generative AI applications with graphs"""
-    )
-    st.divider()
-    streaming_on = st.toggle("Streaming")
-    st.button("Clear Screen", on_click=clear_screen)
+def write_messages(context):
+    for message in st.session_state.messages:
+        if message["role"] == "assistant" or (
+            "context" in message and message["context"] == context
+        ):
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
 
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing your data. This may take a while..."):
-        PERSIST_DIR = "storage_website"
-        graph_store = NeptuneAnalyticsGraphStore(graph_identifier=graph_identifier)
-        # check if storage already exists
-        if not os.path.exists(PERSIST_DIR):
-            # load the documents and create the index
-            loader = SimpleWebPageReader()
-            documents = loader.load_data(
-                [
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/what-is-neptune-analytics.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/doc-history.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/notebooks.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/create-notebook-cfn.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/create-notebook-console.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/create-graph-using-console.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/loading-data.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/loading-data-formats.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/iam-roles-for-loading.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/neptune-load-batch.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/bulk-loading.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/algorithms.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/bfs-standard.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/bfs-parents.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/bfs-levels.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/sssp-bellmanFord.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/sssp-bellmanFord-parents.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/sssp-deltaStepping.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/sssp-deltaStepping-parents.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/topk-sssp.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/degree.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/degree-mutate.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/page-rank.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/page-rank-mutate.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/closeness-centrality.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/closeness-centrality-mutate.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/common-neighbors.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/total-neighbors.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/jaccard-similarity.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/overlap-similarity.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/wcc.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/wcc-mutate.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/label-propagation.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/label-propagation-mutate.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/scc.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/scc-mutate.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vector-index.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vss-algorithms.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vectors-distance.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vectors-get.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vectors-topKByEmbedding.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vectors-topKByNode.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vectors-upsert.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/vectors-remove.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/analytics-limits.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs-execute-query.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs-list-queries.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs-get-query.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs-cancel-query.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs-graph-summary.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-APIs-IAM-role-mappings.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-plan-cache.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-explain.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-statistics.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-exceptions.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-openCypher-data-model.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-openCypher-standards-compliance.html",
-                    "https://docs.aws.amazon.com/neptune-analytics/latest/userguide/query-isolation-level.html",
-                ]
-            )
-            graph_store = NeptuneAnalyticsGraphStore(graph_identifier=graph_identifier)
-            storage_context = StorageContext.from_defaults(graph_store=graph_store)
-            index = KnowledgeGraphIndex.from_documents(
-                documents,
-                storage_context=storage_context,
-                max_triplets_per_chunk=15,
-                include_embeddings=True,
-                show_progress=True,
-            )
-            # persistent storage
-            index.storage_context.persist(persist_dir=PERSIST_DIR)
-        else:
-            # load the existing index
-            storage_context = StorageContext.from_defaults(
-                persist_dir=PERSIST_DIR, graph_store=graph_store
-            )
-            index = load_index_from_storage(storage_context)
-        return index
-
-
-# Create Index
-index = load_data()
-
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [
-        {"role": "assistant", "content": "How may I assist you today?"}
-    ]
-
-# Display or clear chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-# Chat Input - User Prompt
-if prompt := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.write(prompt)
-
-    if streaming_on:
-        # Query Engine - Streaming
-        query_engine = index.as_query_engine(streaming=True)
-        with st.chat_message("assistant"):
-            placeholder = st.empty()
-            full_response = ""
-            streaming_response = query_engine.query(prompt)
-            for chunk in streaming_response.response_gen:
-                full_response += chunk
-                placeholder.markdown(full_response)
-            placeholder.markdown(full_response)
-            st.session_state.messages.append(
-                {"role": "assistant", "content": full_response}
-            )
-
-    else:
-        # Query Engine - Query
-        query_engine = index.as_query_engine()
+def run_query(indices, prompt, context, col):
+    with col:
+        with st.chat_message("user"):
+            st.write(prompt)
+        query_engine = indices[context].as_query_engine()
         with st.chat_message("assistant"):
             with st.spinner("Thinking..."):
                 response = query_engine.query(prompt)
                 st.write(response.response)
                 st.session_state.messages.append(
-                    {"role": "assistant", "content": response.response}
+                    {"role": context, "content": response.response}
                 )
+        st.divider()
+
+        with st.spinner("Evaluating Responses ..."):
+            resp = evaluate_response(prompt, response)
+            st.write(f"**FAITHFULNESS EVALUATION**: {resp['faithfulness']}")
+            st.write(f"**RELEVANCY EVALUATION**: {resp['relevancy']}")
+
+
+def evaluate_response(query, response):
+    faith_result = faithfulness_evaluator.evaluate_response(response=response)
+    relevancy_result = relevancy_evaluator.evaluate_response(
+        query=query, response=response
+    )
+    return {
+        "faithfulness": str(faith_result.passing),
+        "relevancy": str(relevancy_result.passing),
+    }
+
+
+st.title("Neptune-LlamaIndex 🦙 Q&A Chatbot ")
+# st.image("llamaposeidon.png", use_column_width=True)
+st.divider()
+
+
+@st.cache_resource(show_spinner=False)
+def load_indices():
+    with st.spinner(text="Loading and indexing your data. This may take a while..."):
+        return create_or_load_indexes()
+
+
+# with Profiler() as pr:
+
+# Create Index
+indices = load_indices()
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [
+        {
+            "role": "assistant",
+            "content": "How may I assist you today?",
+            "context": "assistant",
+        }
+    ]
+
+
+col1, col2 = st.columns([0.5, 0.5])
+with col1:
+    st.subheader("RAG - similarity using vectors")
+    write_messages("vssindex")
+
+with col2:
+    st.subheader("Graph RAG - similarity using vectors + Knowledge Graph")
+    write_messages("kgindex")
+
+
+if prompt := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    t1 = threading.Thread(target=run_query, args=[indices, prompt, "vss_index", col1])
+    t2 = threading.Thread(target=run_query, args=[indices, prompt, "kg_index", col2])
+    add_script_run_ctx(t1)
+    add_script_run_ctx(t2)
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
